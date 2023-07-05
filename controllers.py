ce45a9689daebb6354fba4aa07d1ac8453f3332a
ThienHoanNeeeee <hoannt@vihatgroup.com>
2023-07-05 15:37:57 +0700

    add

--- controllers.py
+++ controllers.py
@@ -18,5 +18,19 @@ def check_images():
         return response(-9999, 'failed', str(e))
 
 
+@app.route('/check/face_recognition', methods=['POST'])
+def check_images():
+    try:
+        json_data = request.get_json()
+        if not json_data:
+            return response(-9999, 'failed', 'missing_params')
+
+        image_urls = json_data.get('image_urls')
+        defined_image = json_data.get('defined_image')
+        return face_compare(image_urls, defined_image)
+    except AttributeError as e:
+        return response(-9999, 'failed', str(e))
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv('PORT', default=8081))
